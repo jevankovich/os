05508ac13ce9d9208cf712c5f9d1ba295f3601a4
Justin Evankovich <jaevanko@mtu.edu>
2018-12-03 22:30:59 -0500

    Program now prints a string to fb. Proof of concept

--- boot.asm
+++ boot.asm
@@ -5,12 +5,25 @@ bits 32
 _start:
 	mov eax, 0xb8000
 	mov ecx, 0
-clear_vga:
+.clear_vga:
 	mov word [eax + ecx], 0
 	
 	inc ecx
 	cmp ecx, 4000
-	jl clear_vga
+	jl .clear_vga
 
-	mov word [0xb8000], 0x0248
+	mov esi, msg
+	mov edi, 0xb8000
+	mov ax, 0x0700
+.write_msg:
+	lodsb
+	cmp al, 0
+	je .end_msg
+	stosw
+	jmp .write_msg
+.end_msg
 	hlt
+
+section .data
+msg:
+	db 'Red pandas are adorable',0
